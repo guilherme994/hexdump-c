94807dc68099e5fcf9e170738001cf4dc973c02d
guilherme994 <birigui777@gmail.com>
2026-05-18 18:24:20 -0300

    prototipo inicial - le o arquivo todo, ja possui conversao para ASCII.

--- src/main.c
+++ src/main.c
@@ -1,5 +1,34 @@
+#include <stddef.h>
 #include <stdio.h>
+#include <stdint.h>
 
-int main(int argc, char* argv[]) {
-	printf("Hello, world!\n");
+int main() {
+	FILE* f = fopen("src/teste", "rb");
+	if(f == NULL) {
+		perror("fopen");
+		return 1;
+	}
+	int count = 0;
+	uint8_t buffer[16];
+
+	while(1) {
+		size_t read = fread(buffer, 1, 16, f);
+		printf("%07x0: ", count);
+		for(int i = 0; i <= 16; i+= 2) {	
+			printf("%02X%02X ",buffer[i],buffer[i+1]);
+		}
+		for(int i= 0; i <= 16; i++) {	
+			if(buffer[i] >= 0x20 && buffer[i] <= 0x7E) {
+				printf("%c",(char)buffer[i]);
+			}
+			else {
+				printf(".");
+			}
+		}
+		printf("\n");
+		if(read < 16) break;
+		count++;
+	}
+	fclose(f);
+	return 0 ;
 }
