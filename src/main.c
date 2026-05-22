b4043fca16a6418e2b3fcdbcd483bb2ba0db89df
guilherme994 <birigui777@gmail.com>
2026-05-22 19:39:12 -0300

    Adicionada as fufunçoes de -n para limitar numero de elementos, -s para offset de começo e -v para omitir elementos iguais

--- src/main.c
+++ src/main.c
@@ -1,72 +1,127 @@
+#include <features.h>
+#include <bits/getopt_core.h>
 #include <stddef.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <stdint.h>
 #include <unistd.h>
+#include <string.h>
 
 int main(int argc, char* argv[]) {
 	FILE* f_bin;
 	
-	if(argc < 2) f_bin = stdin;
-	else f_bin = fopen(argv[1], "rb");
+	int n_flag = 0;
+	int s_flag = 0;
+	int v_flag = 0;
 	
+	int n_bytes = 0;
+	int s_soffset = 0;
+	size_t offset = 0;
+	
+	uint8_t buffer[16];
+	uint8_t v_buffer[16];
+
+	char ascii[17];
+
+	char *p_soffset;
+	char *p_nbytes;
+
+	int opt;
+	int squeeze = 0;
+	size_t f_read_copy = 0;
+	int first_print = 1;
+
+	while((opt = getopt(argc, argv,"n:s:v"))!= -1){
+		switch (opt) {
+			case 'n':
+				n_flag = 1;
+				n_bytes = strtol(optarg, &p_nbytes, 10);
+				if(optarg == p_nbytes) {
+					printf("Invalid value for -n\n");
+					return 1;
+				}
+				break;
+			case 's':
+				s_flag = 1;
+				s_soffset = strtol(optarg, &p_soffset, 10);
+				if(optarg == p_soffset) {
+					printf("Invalid value for -s\n");
+					return 1;
+				}
+				break;
+			case 'v':
+				v_flag = 1;
+				break;
+		}
+	}
+
+	if(argc < 2) f_bin = stdin;
+	else f_bin = fopen(argv[optind], "rb");
+
 	if(f_bin == NULL) {
 		perror("fopen");
 		return 1;
 	}
-	int n_flag = 0;
-	int n_bytes = 0;
-	size_t offset = 0;
-	uint8_t buffer[16];
-	char ascii[17];
 
-	int opt = getopt(argc, argv, "nsv:");
-
-	printf("%d\n",argc);
-
-	while(opt != -1){
-
-		switch (opt) {
-			case 'n':
-				break;
-
-			case 's':
-				break;
-
-			case 'v':
-				break;
-		}
-	}
+	if(s_flag) fseek(f_bin, s_soffset, SEEK_SET);
 
 	while(1) {
-		size_t f_read = fread(buffer, 1, n_flag ? n_bytes : 16, f_bin);
-		if(f_read == 0) {
-			if(offset > 0) printf("%08zx\n", offset);
-			break;
+		int str_size = 16;
+		if(n_flag == 1 || n_bytes > 16) {
+			if(n_bytes < str_size) str_size = n_bytes;
 		}
-		printf("%08zx  ", offset);
+
+		size_t f_read = fread(buffer, 1, str_size, f_bin);
+
+		int verbose = (f_read == 16 && f_read_copy == 16 && v_flag && memcmp(v_buffer, buffer, sizeof(buffer)) == 0 && !first_print);
+
+		if(!verbose) {
+			if(f_read == 0 && !first_print) {
+				printf("%08zx\n", (offset + f_read + (s_flag ? s_soffset : 0)));
+				break;
+			}
+			printf("%08zx  ", offset);
 		
-		for(size_t i = 0; i < f_read; i++) {	
-			printf("%02x ", buffer[i]);
-			if(buffer[i] >= 0x20 && buffer[i] <= 0x7E) {
-				ascii[i] = (char)buffer[i];
+			for(size_t i = 0; i < f_read; i++) {	
+				printf("%02x ", buffer[i]);
+				if(buffer[i] >= 0x20 && buffer[i] <= 0x7E) {
+					ascii[i] = (char)buffer[i];
+				}
+				else { 
+					ascii[i] = '.';
+				}
+				if(i == 7) printf(" ");
+ 			}
+		
+			if(f_read < 8) printf(" ");
+			
+			for(size_t i = f_read; i < 16; i++){
+				printf("   ");
 			}
-			else { 
-				ascii[i] = '.';
+
+			ascii[f_read] = '\0';
+			printf(" |%s|\n",ascii);
+			
+			if(f_read < 16) {
+				printf("%08zx\n", (offset + f_read + (s_flag ? s_soffset : 0)));
+				break;
 			}
-			if(i == 7) printf(" ");
- 		}
-		ascii[f_read] = '\0';
- 
-		for(size_t i = f_read; i < 16; i++){
-			printf("   ");
+
+			squeeze = 0;
 		}
-
-		printf(" |%s|\n",ascii);
-
-		if(f_read < 16) break;
-
+		else if (!squeeze) {
+			printf("*\n");
+			squeeze = 1;
+		}
+	
 		offset += f_read;
+		f_read_copy = f_read;
+		first_print = 0;
+		
+		if(v_flag) memcpy(v_buffer, buffer, sizeof(buffer));
+		if(n_bytes > str_size) n_bytes -= str_size;
 	}
+
 	if(f_bin != stdin) { 
 		fclose(f_bin);
 	}
